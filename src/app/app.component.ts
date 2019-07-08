6ee21cb191b425c825f8a773ec54fac67e240192
TimonGaebelein <timon.gaebelein@icloud.com>
2019-07-08 19:38:52 +0200

    Some final design tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
   };
   job: Job = {
     filename: "Benchy.gcode",
-    progress: 99,
+    progress: 55,
     filamentAmount: 5.8,
     timeLeft: {
       value: "0:45",
@@ -31,6 +31,26 @@ export class AppComponent {
       unit: "h"
     }
   };
+  // job = null;
+  printHead: PrintHead = {
+    x: 154,
+    y: 201,
+    z: 25.4
+  }
+  printerState: PrinterState = {
+    nozzle: {
+      current: 190,
+      set: 190
+    },
+    heatbed: {
+      current: 56,
+      set: 55
+    },
+    fan: {
+      current: 80,
+      set: 80
+    }
+  }
 
   constructor(private _service: AppService) {
     this._service.getConfig().subscribe((config: Config) => this.config = config);
@@ -53,4 +73,21 @@ interface Job {
   filamentAmount: number;
   timeLeft: Duration;
   timeTotal: Duration;
+}
+
+interface PrintHead {
+  x: number;
+  y: number;
+  z: number;
+}
+
+interface PrinterState {
+  nozzle: PrinterValue;
+  heatbed: PrinterValue;
+  fan: PrinterValue;
+}
+
+interface PrinterValue {
+  current: number;
+  set?: number;
 }
