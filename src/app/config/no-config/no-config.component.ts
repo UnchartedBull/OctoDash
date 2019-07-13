1da58144dd59ad6db52011599eb702d6e04886b1
TimonGaebelein <timon.gaebelein@icloud.com>
2019-07-14 01:17:29 +0200

    Design second screen finished

--- src/app/config/no-config/no-config.component.ts
+++ src/app/config/no-config/no-config.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoConfigComponent implements OnInit {
   page = 1;
+  totalPages = 3;
 
   constructor() { }
 
   ngOnInit() {
+    this.changeProgress()
+  }
+
+  increasePage() {
+    this.page += 1;
+    this.changeProgress()
+  }
+
+  decreasePage() {
+    this.page -= 1;
+    this.changeProgress()
+  }
+
+  changeProgress() {
+    document.getElementById('progressBar').style.width = this.page * (20 / this.totalPages) + 'vw';
   }
 
 }
