7388624233832f369d7721608038b17b3116e137
Harley Glossop <harleyglossop@gmail.com>
2020-03-20 21:01:42 +0000

    Add temperature controls to main menu

--- src/app/printer-status/printer-status.component.ts
+++ src/app/printer-status/printer-status.component.ts
@@ -13,6 +13,10 @@ export class PrinterStatusComponent implements OnInit, OnDestroy {
     private subscriptions: Subscription = new Subscription();
     public printerStatus: PrinterStatus;
     public status: string;
+    public QuickControlView = QuickControlView;
+    public view = QuickControlView.NONE;
+    public newHotendTarget = 200;
+    public newHeatbedTarget = 60;
 
     public constructor(
         private printerService: PrinterService,
@@ -53,10 +57,58 @@ export class PrinterStatusComponent implements OnInit, OnDestroy {
     public ngOnDestroy(): void {
         this.subscriptions.unsubscribe();
     }
+
+    public showQuickControlHotend(): void {
+        this.view = QuickControlView.HOTEND;
+    }
+
+    public showQuickControlHeatbed(): void {
+        this.view = QuickControlView.HEATBED;
+    }
+
+    public hideQuickControl(): void {
+        this.view = QuickControlView.NONE;
+    }
+
+    public changeTemperatureHotend(value: number): void {
+        this.newHotendTarget += value;
+        if (this.newHotendTarget < 0) {
+            this.newHotendTarget = 0;
+        }
+        if (this.newHotendTarget > 999) {
+            this.newHotendTarget = 999;
+        }
+    }
+
+    public changeTemperatureHeatbed(value: number): void {
+        this.newHeatbedTarget += value;
+        if (this.newHeatbedTarget < 0) {
+            this.newHeatbedTarget = 0;
+        }
+        if (this.newHeatbedTarget > 999) {
+            this.newHeatbedTarget = 999;
+        }
+    }
+
+    public setTemperatureHotend(): void {
+        this.printerService.setTemperatureHotend(this.newHotendTarget);
+        this.view = QuickControlView.NONE;
+    }
+
+    public setTemperatureHeatbed(): void {
+        this.printerService.setTemperatureHeatbed(this.newHeatbedTarget);
+        this.view = QuickControlView.NONE;
+    }
 }
 
 export interface PrinterStatus {
     nozzle: PrinterValue;
     heatbed: PrinterValue;
     fan: number | string;
+}
+
+enum QuickControlView {
+    NONE,
+    HOTEND,
+    HEATBED,
 }
