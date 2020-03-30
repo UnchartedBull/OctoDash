6481930e9e52963ec9be35848c1f05283454fe7e
Timon G <timon.gaebelein@icloud.com>
2020-03-30 23:58:45 +0200

    Update install-no-autostart.sh

--- scripts/install-no-autostart.sh
+++ scripts/install-no-autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-release=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
-dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 gir1.2-gnomekeyring-1.0"
+echo "Installing OctoDash"
 
-echo "Installing OctoPrint Plugins"
+release=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
+dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 gir1.2-gnomekeyring-1.0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0"
 
 if [ -d "/home/pi/OctoPrint/venv" ]; then
     DIRECTORY="/home/pi/OctoPrint/venv"
@@ -14,21 +14,25 @@ else
     read -r DIRECTORY
 fi;
 
-"$DIRECTORY"/bin/pip install -q "https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip"
-"$DIRECTORY"/bin/pip install -q "https://github.com/vitormhenrique/OctoPrint-Enclosure/archive/master.zip"
-"$DIRECTORY"/bin/pip install -q "https://github.com/marian42/octoprint-preheat/archive/master.zip"
+echo "Installing OctoPrint Plugins (this might take a while) ..."
+
+"$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip"
+"$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/vitormhenrique/OctoPrint-Enclosure/archive/master.zip"
+"$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/marian42/octoprint-preheat/archive/master.zip"
 if [[ $* == *--ptg* ]]; then
-    "$DIRECTORY"/bin/pip install -q "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
+    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
 fi
 
-echo "Installing Dependencies"
+echo "Installing Dependencies ..."
 sudo apt -qq update
 sudo apt -qq install $dependencies -y
 
-echo "Installing OctoDash"
+echo "Installing OctoDash ..."
 cd ~
 wget -O octodash.deb $release -q --show-progress
 
 sudo dpkg -i octodash.deb
 
 rm octodash.deb
+
+echo "Done. You can start OctoDash with the command 'octodash' now."
