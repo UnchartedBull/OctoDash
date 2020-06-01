edfba1c348f751ac8600442ebdeec3acb1f6df4e
UnchartedBull <timon.gaebelein@icloud.com>
2020-06-02 01:29:22 +0200

    update remove script

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,4 +1,10 @@
 echo "Removing OctoDash ..."
 sudo dpkg -P octodash
 
-echo "OctoDash has been removed :(. Please make sure to update your ~/.xinitrc and ~/.bashrc files!"
+sed -i '/xset s off/d' ~/.xinitrc
+sed -i '/xset s noblank/d' ~/.xinitrc
+sed -i '/xset -dpms/d' ~/.xinitrc
+sed -i '/ratpoison&/d' ~/.xinitrc
+sed -i '/octodash/d' ~/.xinitrc
+
+echo "OctoDash has been removed :(. Please review your ~/.xinitrc and ~/.bashrc files to make sure everything got removed properly!"
