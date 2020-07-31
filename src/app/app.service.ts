2748b17e4553197274b345c73f301513dd935df7
UnchartedBull <timon.gaebelein@icloud.com>
2020-08-01 00:48:08 +0200

    fix config validation

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -35,12 +35,17 @@ export class AppService {
         "Please restart your system. If the issue persists open an issue on GitHub."
       );
     }
-    this.updateError = [];
+    this.updateError = [
+      ".filament should have required property 'useM600'",
+      ".octodash should have required property 'preferPreviewWhilePrinting'",
+    ];
   }
 
   // If the errors can be automatically fixed return true here
   public autoFixError(): boolean {
     const config = this.configService.getCurrentConfig();
+    config.filament.useM600 = false;
+    config.octodash.preferPreviewWhilePrinting = false;
     this.configService.saveConfig(config);
     this.configService.updateConfig();
     return true;
