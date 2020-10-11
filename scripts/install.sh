45fe4c02ef628c4fb912cb85ae683f0c4def2ecc
TimonGaebelein <timon.gaebelein@icloud.com>
2020-10-12 00:04:29 +0200

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -669,7 +669,7 @@ text_input() {
 
 
 releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
-dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd-dev"
+dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd1"
 IFS='/' read -ra version <<< "$releaseURL"
 
 echo "Installing OctoDash "${version[7]}
@@ -684,38 +684,43 @@ elif [ -d "/home/pi/oprint" ]; then
     DIRECTORY="/home/pi/oprint"
 else
     echo "Neither /home/pi/OctoPrint/venv nor /home/pi/oprint can be found."
-    text_input "Please specify OctoPrints full virtualenv path manually (no trailing slash)" DIRECTORY
+    echo "If your OctoPrint instance is running on a different machine just type - in the following prompt."
+    text_input "Please specify OctoPrints full virtualenv path manually (no trailing slash)." DIRECTORY
 fi;
 
-if [ ! -d $DIRECTORY ]; then
+if [ $DIRECTORY == "-" ]; then
+    echo "Not installing any plugins for remote installation. Please make sure to have Display Layer Progress installed."
+elif [ ! -d $DIRECTORY ]; then
     echo "Can't find OctoPrint Installation, please run the script again!"
     exit 1
 fi;
 
-plugins=( 'Display Layer Progress (mandatory)' 'Filament Manager' 'Preheat Button' 'Enclosure' 'Print Time Genius' 'Ultimaker Format Package' 'PrusaSlicer Thumbnails' )
-checkbox_input "Which plugins should I install (you can also install them via the Octoprint UI)?" plugins selected_plugins
-echo "Installing Plugins..."
+if [ $DIRECTORY != "-" ]; then
+  plugins=( 'Display Layer Progress (mandatory)' 'Filament Manager' 'Preheat Button' 'Enclosure' 'Print Time Genius' 'Ultimaker Format Package' 'PrusaSlicer Thumbnails' )
+  checkbox_input "Which plugins should I install (you can also install them via the Octoprint UI)?" plugins selected_plugins
+  echo "Installing Plugins..."
 
-if [[ " ${selected_plugins[@]} " =~ "Display Layer Progress (mandatory)" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "Filament Manager" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/OllisGit/OctoPrint-FilamentManager/releases/latest/download/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "Preheat Button" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/marian42/octoprint-preheat/archive/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "Enclosure" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/vitormhenrique/OctoPrint-Enclosure/archive/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "Print Time Genius" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "Ultimaker Format Package" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/jneilliii/OctoPrint-UltimakerFormatPackage/archive/master.zip"
-fi;
-if [[ " ${selected_plugins[@]} " =~ "PrusaSlicer Thumbnails" ]]; then
-    "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/jneilliii/OctoPrint-PrusaSlicerThumbnails/archive/master.zip"
+  if [[ " ${selected_plugins[@]} " =~ "Display Layer Progress (mandatory)" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "Filament Manager" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/OllisGit/OctoPrint-FilamentManager/releases/latest/download/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "Preheat Button" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/marian42/octoprint-preheat/archive/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "Enclosure" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/vitormhenrique/OctoPrint-Enclosure/archive/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "Print Time Genius" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "Ultimaker Format Package" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/jneilliii/OctoPrint-UltimakerFormatPackage/archive/master.zip"
+  fi;
+  if [[ " ${selected_plugins[@]} " =~ "PrusaSlicer Thumbnails" ]]; then
+      "$DIRECTORY"/bin/pip install -q --disable-pip-version-check "https://github.com/jneilliii/OctoPrint-PrusaSlicerThumbnails/archive/master.zip"
+  fi;
 fi;
 
 echo "Installing OctoDash ..."
