73d42cccf4b83e778dd4924af724cb65b6fe9193
Will-wastelander <71540407+Will-wastelander@users.noreply.github.com>
2020-11-06 00:39:39 -0800

    Update install.sh
    Added additional deps reported here (https://github.com/UnchartedBull/OctoDash/wiki/Troubleshooting#not-starting-automatically)

--- scripts/install.sh
+++ scripts/install.sh
@@ -679,7 +679,7 @@ elif  [[ $arch == arm* ]]; then
     echo "ARM Architecture"
     releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
 fi
-dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd1"
+dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd1 libpam0g-dev libx11-dev"
 IFS='/' read -ra version <<< "$releaseURL"
 
 echo "Installing OctoDash "${version[7]}
