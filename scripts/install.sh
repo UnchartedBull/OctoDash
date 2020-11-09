f9cee6c47589aa4779ee4a878b6a5620563e4c6a
Will-wastelander <71540407+Will-wastelander@users.noreply.github.com>
2020-11-09 15:41:07 -0800

    Update install.sh (#1157)
    * Update install.sh
    
    Addd multi arch support to installer script.
    
    * Update install.sh
    
    Added additional deps reported here (https://github.com/UnchartedBull/OctoDash/wiki/Troubleshooting#not-starting-automatically)
    
    * Update install.sh
    
    Added arch info to Installing OctDash echo line, and removed arch echos.
    
    * Update install.sh
    
    forgot comma
    
    * Update install.sh
    
    Made sure that all echo "Installing OctoDash..." along w/ final echo of successful install provided version and arch info
    
    * Update install.sh
    
    Corrected missing " in echo lines. Removed version and arch from last echo line.

--- scripts/install.sh
+++ scripts/install.sh
@@ -668,11 +668,18 @@ text_input() {
 
 
 
-releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
-dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd1"
+arch=$(uname -m)
+if [[ $arch == x86_64 ]]; then
+    releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*amd64.deb" | cut -d '"' -f 4)
+elif [[ $arch == aarch64 ]]; then
+    releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*arm64.deb" | cut -d '"' -f 4)
+elif  [[ $arch == arm* ]]; then
+    releaseURL=$(curl -s "https://api.github.com/repos/UnchartedBull/OctoDash/releases/latest" | grep "browser_download_url.*armv7l.deb" | cut -d '"' -f 4)
+fi
+dependencies="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libuuid1 libappindicator3-1 libsecret-1-0 xserver-xorg ratpoison x11-xserver-utils xinit libgtk-3-0 bc desktop-file-utils libavahi-compat-libdnssd1 libpam0g-dev libx11-dev"
 IFS='/' read -ra version <<< "$releaseURL"
 
-echo "Installing OctoDash "${version[7]}
+echo "Installing OctoDash "${version[7]}, $arch""
 
 echo "Installing Dependencies ..."
 sudo apt -qq update
@@ -723,7 +730,7 @@ if [ $DIRECTORY != "-" ]; then
   fi;
 fi;
 
-echo "Installing OctoDash ..."
+echo "Installing OctoDash "${version[7]}, $arch" ..."
 cd ~
 wget -O octodash.deb $releaseURL -q --show-progress
 
