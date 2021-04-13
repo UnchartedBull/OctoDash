1ad70515cad1b60a1b38f1706b214d1836396249
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2021-04-13 13:30:21 +0200

    implement printernotification socket event emitter

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -3,8 +3,9 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ConfigService } from './config/config.service';
-import { PrinterEvent } from './model/event.model';
+import { PrinterEvent, PrinterNotification } from './model/event.model';
 import { SocketService } from './services/socket/socket.service';
+import { NotificationService } from './notification/notification.service';
 
 @Injectable()
 export class EventService implements OnDestroy {
@@ -15,33 +16,60 @@ export class EventService implements OnDestroy {
   public constructor(
     private socketService: SocketService,
     private configService: ConfigService,
+    private notificationService: NotificationService,
     private router: Router,
   ) {
     this.subscriptions.add(
-      this.socketService.getEventSubscribable().subscribe((event: PrinterEvent) => {
-        if (event === PrinterEvent.PRINTING || event === PrinterEvent.PAUSED) {
-          setTimeout(() => {
-            this.printing = true;
-          }, 500);
+      this.socketService.getEventSubscribable().subscribe((event: PrinterEvent | PrinterNotification) => {
+        if (this.isPrinterNotification(event)) {
+          this.handlePrinterNotification(event);
         } else {
-          setTimeout(() => {
-            this.printing = false;
-          }, 1000);
-        }
-
-        if (event === PrinterEvent.CLOSED) {
-          this.router.navigate(['/standby']);
-        } else if (event === PrinterEvent.CONNECTED) {
-          setTimeout(() => {
-            if (this.configService.isTouchscreen()) {
-              this.router.navigate(['/main-screen']);
-            } else {
-              this.router.navigate(['/main-screen-no-touch']);
-            }
-          }, 1000);
+          this.handlePrinterEvent(event);
         }
       }),
     );
+  }
+
+  // https://stackoverflow.com/questions/14425568/interface-type-check-with-typescript
+  // https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript/8511350#8511350
+  private isPrinterNotification(object: any): object is PrinterNotification {
+    return typeof object === 'object'
+      && object !== null
+      && ('text' in object || 'message' in object);
+  }
+
+  private handlePrinterNotification(event: PrinterNotification): void {
+    if (event.choices?.length > 0) {
+      // event is action:prompt
+      this.notificationService.setPrompt($localize`:@@action-required:Action required`, event.text, event.choices)
+    } else {
+      // event is action:notification
+      this.notificationService.setInfo($localize`:@@printer-information:Printer information`, event.message)
+    }
+  }
+
+  private handlePrinterEvent(event: PrinterEvent): void {
+    if (event === PrinterEvent.PRINTING || event === PrinterEvent.PAUSED) {
+      setTimeout(() => {
+        this.printing = true;
+      }, 500);
+    } else {
+      setTimeout(() => {
+        this.printing = false;
+      }, 1000);
+    }
+
+    if (event === PrinterEvent.CLOSED) {
+      this.router.navigate(['/standby']);
+    } else if (event === PrinterEvent.CONNECTED) {
+      setTimeout(() => {
+        if (this.configService.isTouchscreen()) {
+          this.router.navigate(['/main-screen']);
+        } else {
+          this.router.navigate(['/main-screen-no-touch']);
+        }
+      }, 1000);
+    }
   }
 
   ngOnDestroy(): void {
--- src/app/model/event.model.ts
+++ src/app/model/event.model.ts
@@ -6,3 +6,10 @@ export enum PrinterEvent {
   IDLE,
   UNKNOWN,
 }
+
+// either notification (message) or prompt (text and choices)
+export interface PrinterNotification {
+  message?: string,
+  text?: string,
+  choices?: string[],
+}
