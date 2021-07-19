8506e4a5bda0e44f77d76bc9359e502e4bf4f413
Martin Dempsey <martind@seeka.co.nz>
2021-07-20 11:06:11 +1200

    Displays latest print success in folder view

--- src/app/model/files.model.ts
+++ src/app/model/files.model.ts
@@ -1,22 +1,24 @@
 export interface Directory {
-  folders: Array<Folder>;
-  files: Array<File>;
+    folders: Array<Folder>;
+    files: Array<File>;
 }
 
 export interface Folder {
-  origin: string;
-  path: string;
-  name: string;
-  size: string;
+    origin: string;
+    path: string;
+    name: string;
+    size: string;
 }
 
 export interface File {
-  origin: string;
-  path: string;
-  name: string;
-  size: string;
-  thumbnail: string;
-  printTime?: string;
-  filamentWeight?: number;
-  date?: string;
+    origin: string;
+    path: string;
+    name: string;
+    size: string;
+    thumbnail: string;
+    successfull: string;
+    successfull_icon: string;
+    printTime?: string;
+    filamentWeight?: number;
+    date?: string;
 }
--- src/app/services/files/files.octoprint.service.ts
+++ src/app/services/files/files.octoprint.service.ts
@@ -12,180 +12,182 @@ import { NotificationService } from '../../notification/notification.service';
 import { FilesService } from './files.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class FilesOctoprintService implements FilesService {
-  private loadedFile = false;
+    private loadedFile = false;
 
-  public constructor(
-    private configService: ConfigService,
-    private notificationService: NotificationService,
-    private http: HttpClient,
-    private conversionService: ConversionService,
-  ) {}
+    public constructor(
+        private configService: ConfigService,
+        private notificationService: NotificationService,
+        private http: HttpClient,
+        private conversionService: ConversionService,
+    ) { }
 
-  public getFolderContent(folderPath?: string): Observable<Directory> {
-    return this.http
-      .get(
-        this.configService.getApiURL('files' + (folderPath === '/' ? '' : folderPath)),
-        this.configService.getHTTPHeaders(),
-      )
-      .pipe(
-        map(response => {
-          if (Object.prototype.hasOwnProperty.call(response, 'children')) {
-            return response['children'];
-          } else {
-            return response['files'];
-          }
-        }),
-        map((folderContent: Array<OctoprintFile & OctoprintFolder>) => {
-          const directory: Directory = { files: [], folders: [] };
-
-          folderContent.forEach(fileOrFolder => {
-            if (fileOrFolder.type === 'folder') {
-              directory.folders.push({
-                origin: fileOrFolder.origin,
-                path: '/' + fileOrFolder.origin + '/' + fileOrFolder.path,
-                name: fileOrFolder.name,
-                size: this.conversionService.convertByteToMegabyte(fileOrFolder.size),
-              } as Folder);
-            }
-
-            if (fileOrFolder.typePath.includes('gcode')) {
-              directory.files.push({
-                origin: fileOrFolder.origin,
-                path: '/' + fileOrFolder.origin + '/' + fileOrFolder.path,
-                name: fileOrFolder.name,
-                date: this.conversionService.convertDateToString(new Date(fileOrFolder.date)),
-                size: this.conversionService.convertByteToMegabyte(fileOrFolder.size),
-                ...(fileOrFolder.gcodeAnalysis
-                  ? {
-                      printTime: this.conversionService.convertSecondsToHours(
-                        fileOrFolder.gcodeAnalysis.estimatedPrintTime,
-                      ),
-                      filamentWeight: this.conversionService.convertFilamentLengthToWeight(
-                        _.sumBy(_.values(fileOrFolder.gcodeAnalysis.filament), tool => tool.length),
-                      ),
+    public getFolderContent(folderPath?: string): Observable<Directory> {
+        return this.http
+            .get(
+                this.configService.getApiURL('files' + (folderPath === '/' ? '' : folderPath)),
+                this.configService.getHTTPHeaders(),
+            )
+            .pipe(
+                map(response => {
+                    if (Object.prototype.hasOwnProperty.call(response, 'children')) {
+                        return response['children'];
+                    } else {
+                        return response['files'];
                     }
-                  : {}),
-              } as File);
-            }
-          });
+                }),
+                map((folderContent: Array<OctoprintFile & OctoprintFolder>) => {
+                    const directory: Directory = { files: [], folders: [] };
 
-          return directory;
-        }),
-        map((directory: Directory) => {
-          if (folderPath === '/') {
-            const localCount = _.sumBy(_.concat(directory.files, directory.folders), (fileOrFolder: File & Folder) =>
-              fileOrFolder.origin === 'local' ? 1 : 0,
+                    folderContent.forEach(fileOrFolder => {
+                        if (fileOrFolder.type === 'folder') {
+                            directory.folders.push({
+                                origin: fileOrFolder.origin,
+                                path: '/' + fileOrFolder.origin + '/' + fileOrFolder.path,
+                                name: fileOrFolder.name,
+                                size: this.conversionService.convertByteToMegabyte(fileOrFolder.size),
+                            } as Folder);
+                        }
+
+                        if (fileOrFolder.typePath.includes('gcode')) {
+                            directory.files.push({
+                                origin: fileOrFolder.origin,
+                                path: '/' + fileOrFolder.origin + '/' + fileOrFolder.path,
+                                name: fileOrFolder.name,
+                                date: this.conversionService.convertDateToString(new Date(fileOrFolder.date)),
+                                size: this.conversionService.convertByteToMegabyte(fileOrFolder.size),
+                                ...(fileOrFolder.gcodeAnalysis
+                                    ? {
+                                        successfull: fileOrFolder.prints != null ? fileOrFolder.prints.last.success ? 'mat-ripple files__object_success' : 'mat-ripple files__object_failed' : 'mat-ripple files__object',
+                                        successfull_icon: fileOrFolder.prints != null ? fileOrFolder.prints.last.success ? 'assets/confirm-small.svg' : 'assets/cancel-small.svg' : fileOrFolder.thumbnail ? this.configService.getApiURL(fileOrFolder.thumbnail, false) : 'assets/object.svg',
+                                        printTime: this.conversionService.convertSecondsToHours(
+                                            fileOrFolder.gcodeAnalysis.estimatedPrintTime,
+                                        ),
+                                        filamentWeight: this.conversionService.convertFilamentLengthToWeight(
+                                            _.sumBy(_.values(fileOrFolder.gcodeAnalysis.filament), tool => tool.length),
+                                        ),
+                                    }
+                                    : {}),
+                            } as File);
+                        }
+                    });
+
+                    return directory;
+                }),
+                map((directory: Directory) => {
+                    if (folderPath === '/') {
+                        const localCount = _.sumBy(_.concat(directory.files, directory.folders), (fileOrFolder: File & Folder) =>
+                            fileOrFolder.origin === 'local' ? 1 : 0,
+                        );
+                        const sdCardCount = _.sumBy(_.concat(directory.files, directory.folders), (fileOrFolder: File & Folder) =>
+                            fileOrFolder.origin === 'sdcard' ? 1 : 0,
+                        );
+
+                        if (localCount > 0 && sdCardCount > 0) {
+                            directory.folders = [
+                                {
+                                    origin: 'local',
+                                    path: '/local',
+                                    name: $localize`:@@local:local`,
+                                    size: undefined,
+                                },
+                                {
+                                    origin: 'sdcard',
+                                    path: '/sdcard',
+                                    name: $localize`:@@sdcard:sdcard`,
+                                    size: undefined,
+                                },
+                            ];
+                            directory.files = [];
+                        }
+                    }
+
+                    return directory;
+                }),
             );
-            const sdCardCount = _.sumBy(_.concat(directory.files, directory.folders), (fileOrFolder: File & Folder) =>
-              fileOrFolder.origin === 'sdcard' ? 1 : 0,
-            );
+    }
 
-            if (localCount > 0 && sdCardCount > 0) {
-              directory.folders = [
-                {
-                  origin: 'local',
-                  path: '/local',
-                  name: $localize`:@@local:local`,
-                  size: undefined,
-                },
-                {
-                  origin: 'sdcard',
-                  path: '/sdcard',
-                  name: $localize`:@@sdcard:sdcard`,
-                  size: undefined,
-                },
-              ];
-              directory.files = [];
-            }
-          }
+    public getFile(filePath: string): Observable<File> {
+        return this.http.get(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders()).pipe(
+            map((file: OctoprintFile): File => {
+                return {
+                    origin: file.origin,
+                    path: '/' + file.origin + '/' + file.path,
+                    name: file.name,
+                    date: this.conversionService.convertDateToString(new Date(file.date * 1000)),
+                    size: this.conversionService.convertByteToMegabyte(file.size),
+                    thumbnail: file.thumbnail ? this.configService.getApiURL(file.thumbnail, false) : 'assets/object.svg',
+                    ...(file.gcodeAnalysis
+                        ? {
+                            printTime: this.conversionService.convertSecondsToHours(file.gcodeAnalysis.estimatedPrintTime),
+                            filamentWeight: this.conversionService.convertFilamentLengthToWeight(
+                                _.sumBy(_.values(file.gcodeAnalysis.filament), tool => tool.length),
+                            ),
+                        }
+                        : {}),
+                } as File;
+            }),
+        );
+    }
 
-          return directory;
-        }),
-      );
-  }
+    public getThumbnail(filePath: string): Observable<string> {
+        return this.http.get(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders()).pipe(
+            map((file: OctoprintFile): string => {
+                return file.thumbnail ? this.configService.getApiURL(file.thumbnail, false) : 'assets/object.svg';
+            }),
+        );
+    }
 
-  public getFile(filePath: string): Observable<File> {
-    return this.http.get(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders()).pipe(
-      map((file: OctoprintFile): File => {
-        return {
-          origin: file.origin,
-          path: '/' + file.origin + '/' + file.path,
-          name: file.name,
-          date: this.conversionService.convertDateToString(new Date(file.date * 1000)),
-          size: this.conversionService.convertByteToMegabyte(file.size),
-          thumbnail: file.thumbnail ? this.configService.getApiURL(file.thumbnail, false) : 'assets/object.svg',
-          ...(file.gcodeAnalysis
-            ? {
-                printTime: this.conversionService.convertSecondsToHours(file.gcodeAnalysis.estimatedPrintTime),
-                filamentWeight: this.conversionService.convertFilamentLengthToWeight(
-                  _.sumBy(_.values(file.gcodeAnalysis.filament), tool => tool.length),
+    public loadFile(filePath: string): void {
+        const payload: FileCommand = {
+            command: 'select',
+            print: false,
+        };
+
+        this.http
+            .post(this.configService.getApiURL('files' + filePath), payload, this.configService.getHTTPHeaders())
+            .pipe(
+                catchError(error =>
+                    this.notificationService.setError($localize`:@@files-error-file:Can't load file!`, error.message),
                 ),
-              }
-            : {}),
-        } as File;
-      }),
-    );
-  }
+            )
+            .subscribe();
+    }
 
-  public getThumbnail(filePath: string): Observable<string> {
-    return this.http.get(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders()).pipe(
-      map((file: OctoprintFile): string => {
-        return file.thumbnail ? this.configService.getApiURL(file.thumbnail, false) : 'assets/object.svg';
-      }),
-    );
-  }
+    public printFile(filePath: string): void {
+        const payload: FileCommand = {
+            command: 'select',
+            print: true,
+        };
 
-  public loadFile(filePath: string): void {
-    const payload: FileCommand = {
-      command: 'select',
-      print: false,
-    };
+        this.http
+            .post(this.configService.getApiURL('files' + filePath), payload, this.configService.getHTTPHeaders())
+            .pipe(
+                catchError(error =>
+                    this.notificationService.setError($localize`:@@files-error-print:Can't start print!`, error.message),
+                ),
+            )
+            .subscribe();
+    }
 
-    this.http
-      .post(this.configService.getApiURL('files' + filePath), payload, this.configService.getHTTPHeaders())
-      .pipe(
-        catchError(error =>
-          this.notificationService.setError($localize`:@@files-error-file:Can't load file!`, error.message),
-        ),
-      )
-      .subscribe();
-  }
+    public deleteFile(filePath: string): void {
+        this.http
+            .delete(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders())
+            .pipe(
+                catchError(error =>
+                    this.notificationService.setError($localize`:@@files-error-delete:Can't delete file!`, error.message),
+                ),
+            )
+            .subscribe();
+    }
 
-  public printFile(filePath: string): void {
-    const payload: FileCommand = {
-      command: 'select',
-      print: true,
-    };
+    public setLoadedFile(value: boolean): void {
+        this.loadedFile = value;
+    }
 
-    this.http
-      .post(this.configService.getApiURL('files' + filePath), payload, this.configService.getHTTPHeaders())
-      .pipe(
-        catchError(error =>
-          this.notificationService.setError($localize`:@@files-error-print:Can't start print!`, error.message),
-        ),
-      )
-      .subscribe();
-  }
-
-  public deleteFile(filePath: string): void {
-    this.http
-      .delete(this.configService.getApiURL('files' + filePath), this.configService.getHTTPHeaders())
-      .pipe(
-        catchError(error =>
-          this.notificationService.setError($localize`:@@files-error-delete:Can't delete file!`, error.message),
-        ),
-      )
-      .subscribe();
-  }
-
-  public setLoadedFile(value: boolean): void {
-    this.loadedFile = value;
-  }
-
-  public getLoadedFile(): boolean {
-    return this.loadedFile;
-  }
+    public getLoadedFile(): boolean {
+        return this.loadedFile;
+    }
 }
