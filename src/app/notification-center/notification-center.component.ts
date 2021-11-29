d00886172e49e68610a7e8f240cf590e7086fbd8
UnchartedBull <unchartedbull@gmail.com>
2021-11-29 16:03:50 +0100

    add clock & backdrop

--- src/app/notification-center/notification-center.component.ts
+++ src/app/notification-center/notification-center.component.ts
@@ -7,4 +7,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class NotificationCenterComponent {
   @Output() hideNotificationCenter = new EventEmitter<void>();
+
+  public time: string;
+
+  constructor() {
+    this.updateTime();
+  }
+
+  public updateTime(): void {
+    const now = new Date();
+    this.time = `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
+    setTimeout(this.updateTime, 5000);
+  }
 }
