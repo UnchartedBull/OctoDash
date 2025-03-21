27a6f5af1fef9ee9524e6f400f807183d3c2ceba
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2025-03-21 00:41:13 -0700

    Move some components around

---
+++ src/app/main-screen/main-menu/main-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.scss'],
+})
+export class MainMenuComponent {
+  public constructor() {}
+}
