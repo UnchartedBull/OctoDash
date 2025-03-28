ba3ac0c00b3e90cb8869ef9b86a8359cc4fdd36f
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2025-03-28 15:41:46 -0700

    Define service providers in their own file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatRippleModule } from '@angular/material/core';
@@ -11,28 +11,9 @@ import { LottieComponent, provideCacheableAnimationLoader, provideLottieOptions 
 
 import components from './components';
 import { AppComponent } from './components/app.component';
+import services from './services';
 import { AppRoutingModule } from './app.routing.module';
-import { AppService } from './services/app.service';
-import { ConfigService } from './services/config.service';
-import { ConversionService } from './services/conversion.service';
-import { EventService } from './services/event.service';
 import { LongPress } from './long-press.directive';
-import { NotificationService } from './services/notification.service';
-import { EnclosureOctoprintService } from './services/enclosure/enclosure.octoprint.service';
-import { EnclosureService } from './services/enclosure/enclosure.service';
-import { FilamentManagerOctoprintService } from './services/filament/filament-manager.octoprint.service';
-import { FilamentPluginService } from './services/filament/filament-plugin.service';
-import { SpoolManagerOctoprintService } from './services/filament/spool-manager.octoprint.service';
-import { FilesOctoprintService } from './services/files/files.octoprint.service';
-import { FilesService } from './services/files/files.service';
-import { JobOctoprintService } from './services/job/job.octoprint.service';
-import { JobService } from './services/job/job.service';
-import { PrinterOctoprintService } from './services/printer/printer.octoprint.service';
-import { PrinterService } from './services/printer/printer.service';
-import { OctoPrintSocketService } from './services/socket/socket.octoprint.service';
-import { SocketService } from './services/socket/socket.service';
-import { SystemOctoprintService } from './services/system/system.octoprint.service';
-import { SystemService } from './services/system/system.service';
 import { URLSafePipe } from './url.pipe';
 
 @NgModule({
@@ -49,110 +30,7 @@ import { URLSafePipe } from './url.pipe';
     LottieComponent,
   ],
   providers: [
-    AppService,
-    ConfigService,
-    ConversionService,
-    EventService,
-    NotificationService,
-    [
-      {
-        provide: SystemService,
-        deps: [ConfigService, NotificationService, HttpClient],
-        useFactory: (
-          configService: ConfigService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-        ) => {
-          return new SystemOctoprintService(configService, notificationService, httpClient);
-        },
-      },
-    ],
-    [
-      {
-        provide: SocketService,
-        deps: [ConfigService, SystemService, ConversionService, NotificationService, HttpClient],
-        useFactory: (
-          configService: ConfigService,
-          systemService: SystemService,
-          conversionService: ConversionService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-        ) => {
-          return new OctoPrintSocketService(
-            configService,
-            systemService,
-            conversionService,
-            notificationService,
-            httpClient,
-          );
-        },
-      },
-    ],
-    [
-      {
-        provide: PrinterService,
-        deps: [ConfigService, NotificationService, HttpClient],
-        useFactory: (
-          configService: ConfigService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-        ) => {
-          return new PrinterOctoprintService(configService, notificationService, httpClient);
-        },
-      },
-    ],
-    [
-      {
-        provide: JobService,
-        deps: [ConfigService, NotificationService, HttpClient],
-        useFactory: (
-          configService: ConfigService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-        ) => {
-          return new JobOctoprintService(configService, notificationService, httpClient);
-        },
-      },
-    ],
-    [
-      {
-        provide: FilesService,
-        deps: [ConfigService, NotificationService, HttpClient, ConversionService],
-        useFactory: (
-          configService: ConfigService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-          conversionService: ConversionService,
-        ) => {
-          return new FilesOctoprintService(configService, notificationService, httpClient, conversionService);
-        },
-      },
-    ],
-    [
-      {
-        provide: FilamentPluginService,
-        deps: [ConfigService, HttpClient],
-        useFactory: (configService: ConfigService, httpClient: HttpClient) => {
-          if (configService.isSpoolManagerPluginEnabled()) {
-            return new SpoolManagerOctoprintService(configService, httpClient);
-          }
-          return new FilamentManagerOctoprintService(configService, httpClient);
-        },
-      },
-    ],
-    [
-      {
-        provide: EnclosureService,
-        deps: [ConfigService, NotificationService, HttpClient],
-        useFactory: (
-          configService: ConfigService,
-          notificationService: NotificationService,
-          httpClient: HttpClient,
-        ) => {
-          return new EnclosureOctoprintService(configService, notificationService, httpClient);
-        },
-      },
-    ],
+    ...services,
     [provideLottieOptions({ player: () => lottiePlayer })],
     [provideCacheableAnimationLoader()],
     provideHttpClient(withInterceptorsFromDi()),
---
+++ src/app/services/index.ts
@@ -0,0 +1,113 @@
+import { HttpClient } from '@angular/common/http';
+
+import { AppService } from './app.service';
+import { ConfigService } from './config.service';
+import { ConversionService } from './conversion.service';
+import { EventService } from './event.service';
+import { NotificationService } from './notification.service';
+import { EnclosureOctoprintService } from './enclosure/enclosure.octoprint.service';
+import { EnclosureService } from './enclosure/enclosure.service';
+import { FilamentManagerOctoprintService } from './filament/filament-manager.octoprint.service';
+import { FilamentPluginService } from './filament/filament-plugin.service';
+import { SpoolManagerOctoprintService } from './filament/spool-manager.octoprint.service';
+import { FilesOctoprintService } from './files/files.octoprint.service';
+import { FilesService } from './files/files.service';
+import { JobOctoprintService } from './job/job.octoprint.service';
+import { JobService } from './job/job.service';
+import { PrinterOctoprintService } from './printer/printer.octoprint.service';
+import { PrinterService } from './printer/printer.service';
+import { OctoPrintSocketService } from './socket/socket.octoprint.service';
+import { SocketService } from './socket/socket.service';
+import { SystemOctoprintService } from './system/system.octoprint.service';
+import { SystemService } from './system/system.service';
+
+export default [
+  AppService,
+  ConfigService,
+  ConversionService,
+  EventService,
+  NotificationService,
+  [
+    {
+      provide: SystemService,
+      deps: [ConfigService, NotificationService, HttpClient],
+      useFactory: (configService: ConfigService, notificationService: NotificationService, httpClient: HttpClient) => {
+        return new SystemOctoprintService(configService, notificationService, httpClient);
+      },
+    },
+  ],
+  [
+    {
+      provide: SocketService,
+      deps: [ConfigService, SystemService, ConversionService, NotificationService, HttpClient],
+      useFactory: (
+        configService: ConfigService,
+        systemService: SystemService,
+        conversionService: ConversionService,
+        notificationService: NotificationService,
+        httpClient: HttpClient,
+      ) => {
+        return new OctoPrintSocketService(
+          configService,
+          systemService,
+          conversionService,
+          notificationService,
+          httpClient,
+        );
+      },
+    },
+  ],
+  [
+    {
+      provide: PrinterService,
+      deps: [ConfigService, NotificationService, HttpClient],
+      useFactory: (configService: ConfigService, notificationService: NotificationService, httpClient: HttpClient) => {
+        return new PrinterOctoprintService(configService, notificationService, httpClient);
+      },
+    },
+  ],
+  [
+    {
+      provide: JobService,
+      deps: [ConfigService, NotificationService, HttpClient],
+      useFactory: (configService: ConfigService, notificationService: NotificationService, httpClient: HttpClient) => {
+        return new JobOctoprintService(configService, notificationService, httpClient);
+      },
+    },
+  ],
+  [
+    {
+      provide: FilesService,
+      deps: [ConfigService, NotificationService, HttpClient, ConversionService],
+      useFactory: (
+        configService: ConfigService,
+        notificationService: NotificationService,
+        httpClient: HttpClient,
+        conversionService: ConversionService,
+      ) => {
+        return new FilesOctoprintService(configService, notificationService, httpClient, conversionService);
+      },
+    },
+  ],
+  [
+    {
+      provide: FilamentPluginService,
+      deps: [ConfigService, HttpClient],
+      useFactory: (configService: ConfigService, httpClient: HttpClient) => {
+        if (configService.isSpoolManagerPluginEnabled()) {
+          return new SpoolManagerOctoprintService(configService, httpClient);
+        }
+        return new FilamentManagerOctoprintService(configService, httpClient);
+      },
+    },
+  ],
+  [
+    {
+      provide: EnclosureService,
+      deps: [ConfigService, NotificationService, HttpClient],
+      useFactory: (configService: ConfigService, notificationService: NotificationService, httpClient: HttpClient) => {
+        return new EnclosureOctoprintService(configService, notificationService, httpClient);
+      },
+    },
+  ],
+];
