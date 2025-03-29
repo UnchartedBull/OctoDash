ff90c4877083108fe49bf071ee9f843f1135c0dc
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2025-03-29 07:03:00 -0700

    Simplify function

--- src/components/control/custom-actions/custom-actions.component.ts
+++ src/components/control/custom-actions/custom-actions.component.ts
@@ -9,6 +9,8 @@ import { NotificationService } from '../../../services/notification.service';
 import { PrinterService } from '../../../services/printer/printer.service';
 import { SystemService } from '../../../services/system/system.service';
 
+const SpecialCommandRegex = /\[!([\w_]+)\]/;
+
 @Component({
   selector: 'app-custom-actions',
   templateUrl: './custom-actions.component.html',
@@ -23,6 +25,23 @@ export class CustomActionsComponent {
   public iframeURL: SafeResourceUrl = 'about:blank';
   public iframeOpen = false;
   public actionToConfirm: ActionToConfirm;
+
+  private commands = {
+    DISCONNECT: () => this.disconnectPrinter(),
+    STOPDASHBOARD: () => this.stopOctoDash(),
+    RELOAD: () => this.reloadOctoPrint(),
+    REBOOT: () => this.rebootPi(),
+    SHUTDOWN: () => this.shutdownPi(),
+    KILL: () => this.kill(),
+    POWEROFF: () => this.enclosureService.setPSUState(PSUState.OFF),
+    POWERON: () => this.enclosureService.setPSUState(PSUState.ON),
+    POWERTOGGLE: () => this.enclosureService.togglePSU(),
+    WEB: value => this.openIframe(value),
+    NEOPIXEL: (...values) => this.setLEDColor(values[0], values[1], values[2], values[3]),
+    OUTPUT: (...values) => this.setOutput(values[0], values[1]),
+    OUTPUT_PWM: (...values) => this.setOutputPWM(values[0], values[1]),
+    ENC_SHELL: value => this.runEnclosureShell(value),
+  };
 
   constructor(
     private printerService: PrinterService,
@@ -55,63 +74,33 @@ export class CustomActionsComponent {
   }
 
   private executeGCode(command: string): void {
-    switch (command) {
-      case '[!DISCONNECT]':
-        this.disconnectPrinter();
-        break;
-      case '[!STOPDASHBOARD]':
-        this.stopOctoDash();
-        break;
-      case '[!RELOAD]':
-        this.reloadOctoPrint();
-        break;
-      case '[!REBOOT]':
-        this.rebootPi();
-        break;
-      case '[!SHUTDOWN]':
-        this.shutdownPi();
-        break;
-      case '[!KILL]':
-        this.kill();
-        break;
-      case '[!POWEROFF]':
-        this.enclosureService.setPSUState(PSUState.OFF);
-        break;
-      case '[!POWERON]':
-        this.enclosureService.setPSUState(PSUState.ON);
-        break;
-      case '[!POWERTOGGLE]':
-        this.enclosureService.togglePSU();
-        break;
-      default: {
-        if (command.includes('[!WEB]')) {
-          this.openIframe(command.replace('[!WEB]', ''));
-        } else if (command.includes('[!NEOPIXEL]')) {
-          const values = command.replace('[!NEOPIXEL]', '').split(',');
-          this.setLEDColor(values[0], values[1], values[2], values[3]);
-        } else if (command.includes('[!OUTPUT]')) {
-          const values = command.replace('[!OUTPUT]', '').split(',');
-          this.setOutput(values[0], values[1]);
-        } else if (command.includes('[!OUTPUT_PWM]')) {
-          const values = command.replace('[!OUTPUT_PWM]', '').split(',');
-          this.setOutputPWM(values[0], values[1]);
-        } else if (command.includes('[!ENC_SHELL]')) {
-          const values = command.replace('[!ENC_SHELL]', '').split(',');
-          this.runEnclosureShell(values[0]);
-        } else {
-          if (this.disablePrinterCommands) {
-            this.notificationService.setNotification({
-              heading: $localize`:@@error-custom-action-disabled:Printer commands are not available!`,
-              text: $localize`:@@error-custom-action-disabled-desc:Please connect to your printer first before attempting to use a printer command.`,
-              type: NotificationType.ERROR,
-              time: new Date(),
-            });
-          } else {
-            this.printerService.executeGCode(command);
-          }
-        }
-        break;
+    if (command.startsWith('[!')) {
+      const specialCommand = command.match(SpecialCommandRegex)[0];
+      const values = command.replace(SpecialCommandRegex, '').split(',');
+
+      if (!(specialCommand in this.commands)) {
+        this.notificationService.setNotification({
+          heading: $localize`:@@error-custom-action-invalid:Unknown special command!`,
+          text: $localize`:@@error-custom-action-invalid-desc:The special command you have entered is unknown.`,
+          type: NotificationType.ERROR,
+          time: new Date(),
+        });
+        return;
       }
+
+      this.commands[specialCommand](...values);
+      return;
+    }
+
+    if (this.disablePrinterCommands) {
+      this.notificationService.setNotification({
+        heading: $localize`:@@error-custom-action-disabled:Printer commands are not available!`,
+        text: $localize`:@@error-custom-action-disabled-desc:Please connect to your printer first before attempting to use a printer command.`,
+        type: NotificationType.ERROR,
+        time: new Date(),
+      });
+    } else {
+      this.printerService.executeGCode(command);
     }
   }
 
