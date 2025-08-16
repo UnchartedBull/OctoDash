731d438a71c90668de436128d178d34cf24cd6fc
Hilton Shumway <hillshum@gmail.com>
2025-08-16 19:14:56 +0100

    Prompt users for URL on browser install
    Includes updating the `inquirer.sh` script to allow for prompting with defaults and then also restructuring for more modularity

---
+++ scripts/constants.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+octoprint_suffix="plugin/octodash/"
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,26 +3,26 @@
 # Version 3 moves away from a .deb package to delivering everything as an OctoPrint plugin,
 # which means we just need to point a web broswer to the appropriate place.
 
+# find the directory that this script is in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-echo "Installing Dependencies..."
-{
-    sudo apt -qq update
-    sudo apt -qq install chromium-browser -y
-} || {
-  echo ""
-  echo "Couldn't install dependenices!"
-  echo "Seems like there is something wrong with the package manager 'apt'"
-  echo ""
-  echo "If the error is similar to: 'E: Repository 'http://raspbian.raspberrypi.org/raspbian buster InRelease' changed its 'Suite' value from 'stable' to 'oldstable''"
-  echo "you can run 'sudo apt update --allow-releaseinfo-change' and then execute the OctoDash installation command again"
-  exit -1
-}
+source $DIR/functions.sh
 
+text_input "What is the URL of your OctoPrint installation? Be sure in include a trailing slash" octoprint_url "http://localhost:5000/"
+
+echo "Installing chromium-browser ..."
+install-apt "chromium-browser"
 
 echo "Updating .xinitrc to launch Chromium..."
 
 XINITRC="$HOME/.xinitrc"
 if grep -q "octodash" "$XINITRC"; then
-    sed -i 's/^octodash.*$/chromium-browser --kiosk http:\/\/localhost:5000/' "$XINITRC"
+    sed -i "s@^octodash.*$@chromium-browser --kiosk $octoprint_url$octoprint_suffix@" "$XINITRC"
     echo ".xinitrc updated: replaced 'octodash' with Chromium launch command."
 fi
