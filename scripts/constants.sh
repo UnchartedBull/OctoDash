1a3813f87651b221996db1adfe9b47c894652c02
Hilton Shumway <hillshum@gmail.com>
2025-08-21 20:25:53 +0100

    Begin consolidation of bash scripts
    To ease deployment etc, consolidating everything into a single script. The migrate function is next

--- scripts/constants.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-octodash_url_prompt="What is the URL of your OctoDash installation? Be sure to include a trailing slash. If you don't know, push enter to accept the default."
-octodash_url_default="http://localhost:5000/"
-
-browser_launch_string="chromium-browser --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized" 
-octoprint_suffix="plugin/octodash/"
