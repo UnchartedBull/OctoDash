56eb89db62b77802afa6f8774eb60cf098c405ba
Hilton Shumway <hillshum@gmail.com>
2025-10-27 14:37:23 +0000

    Add error handling on sleep

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -67,7 +67,10 @@ export class AppService {
   }
 
   public turnDisplayOff(): void {
-    this.http.post('/plugin/octodash/api/screen_sleep', {}, this.configService.getHTTPHeaders()).subscribe();
+    this.http.post('/plugin/octodash/api/screen_sleep', {}, this.configService.getHTTPHeaders()).subscribe({
+      error: error =>
+        this.notificationService.error($localize`:@@screen-sleep-error:Error turning display off`, error.message),
+    });
   }
 
   public turnDisplayOn(): void {
