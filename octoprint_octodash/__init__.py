abbbfb9a30332a84696adbfb2bbe1d47f14230cb
Hilton Shumway <hillshum@gmail.com>
2025-11-09 14:15:38 +0000

    Add `SettingsPlugin` mixin back

--- octoprint_octodash/__init__.py
+++ octoprint_octodash/__init__.py
@@ -24,6 +24,7 @@ from octoprint.events import Events
 class OctodashPlugin(
     octoprint.plugin.UiPlugin,
     octoprint.plugin.BlueprintPlugin,
+    octoprint.plugin.SettingsPlugin,
     octoprint.plugin.EventHandlerPlugin,
 ):
 
