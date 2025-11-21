edc72bd4e8b1d85722aa951e6f9356ee7cdc1aca
Hilton Shumway <hillshum@gmail.com>
2025-11-21 17:48:29 +0000

    Don't use observable in fan picker
    No need with this static array

--- src/components/shared/fan-quick-control-component-copy/fan-quick-control.component.ts
+++ src/components/shared/fan-quick-control-component-copy/fan-quick-control.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
-import { of } from 'rxjs';
 import { ConfigService } from 'src/services/config.service';
 import { PrinterService } from 'src/services/printer/printer.service';
 import { ProfileService } from 'src/services/profile/profile.service';
@@ -14,10 +13,10 @@ export class FanQuickControlComponent {
   public profileService = inject(ProfileService);
   public printerService = inject(PrinterService);
 
-  options$ = of([
+  options = [
     { value: 0, label: 'Off' },
     { value: this.configService.getDefaultFanSpeed(), label: 'On' },
-  ]);
+  ];
   onSet(value) {
     this.printerService.setFanSpeed(value);
   }
