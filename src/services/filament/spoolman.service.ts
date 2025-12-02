8513d1926264e2dfbf67272be067936b80ad30ad
Hilton Shumway <hillshum@gmail.com>
2025-12-02 21:37:48 +0000

    Lint fix

--- src/services/filament/spoolman.service.ts
+++ src/services/filament/spoolman.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { forkJoin, Observable, of } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { FilamentSpool } from '../../model';
