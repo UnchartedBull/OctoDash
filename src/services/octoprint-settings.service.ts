ba2339486349406948fc6e06aa7c17e88679b140
Hilton Shumway <hillshum@gmail.com>
2025-12-19 18:11:14 +0000

    Add some caching of the OctoPrint settings for temp pickers (#5552)
    * Use cached OctoPrint settings for temps
    
    New service holds the settings
    
    * Automatically reload OctoPrint settings on change
    
    * Make the initalized subscribable read-only
    
    * Locale update
    
    * Create an initializer that loads settings
    
    * Make the public settings access read-only

---
+++ src/services/octoprint-settings.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { OctoPrintSettings } from 'src/model/octoprint/octoprint-settings.model';
+
+import { ConfigService } from './config.service';
+import { SocketService } from './socket/socket.service';
+
+@Injectable({ providedIn: 'root' })
+export class OctoprintSettingsService {
+  private http = inject(HttpClient);
+  private configService = inject(ConfigService);
+  private socketService = inject(SocketService);
+
+  private settings$ = new BehaviorSubject<OctoPrintSettings | null>(null);
+
+  getSettings() {
+    return this.settings$.asObservable();
+  }
+
+  constructor() {
+    this.configService.getInitializedSubscribable().subscribe(initialized => {
+      if (initialized) {
+        this.loadSettings();
+      }
+    });
+    this.socketService.getSettingsUpdatedSubscribable().subscribe(() => {
+      this.loadSettings();
+    });
+  }
+
+  loadSettings() {
+    const apiUrl = this.configService.getApiURL('settings', true);
+    if (!apiUrl) {
+      console.warn('No API URL found for OctoPrint settings.');
+      return;
+    }
+    this.http.get<OctoPrintSettings>(apiUrl, this.configService.getHTTPHeaders()).subscribe({
+      next: settings => this.settings$.next(settings),
+      error: error => console.error('Error loading OctoPrint settings:', error),
+    });
+  }
+}
