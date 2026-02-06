597f5c1d1b7d36396f352d3ef10d0ca919772809
Hilton Shumway <hillshum@gmail.com>
2026-02-06 08:37:40 +0000

    Use `super` in settings hoooks
    Instead of explicitly naming the parent

--- octoprint_octodash/__init__.py
+++ octoprint_octodash/__init__.py
@@ -52,7 +52,7 @@ class OctodashPlugin(
         installed_plugins = set(manager.enabled_plugins.keys())
 
         
-        data = octoprint.plugin.SettingsPlugin.on_settings_load(self)
+        data = super().on_settings_load(self)
 
         for plugin_name in ALL_PLUGINS.keys():
                 enabled = data["plugins"][plugin_name]["enabled"]
@@ -65,7 +65,7 @@ class OctodashPlugin(
         for plugin_name in ALL_PLUGINS.keys():
             del data["plugins"][plugin_name]['inUse']
 
-        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        return super().on_settings_save(self, data)
 
     def on_settings_migrate(self, target, current):
         self._set_initial_plugins()
