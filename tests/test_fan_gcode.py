ecf83569d7b87e8c7e6c1aa9447f834effa7e5a5
Hilton Shumway <hillshum@gmail.com>
2026-02-11 19:37:14 +0000

    Use parameterize for remaining tests

--- tests/test_fan_gcode.py
+++ tests/test_fan_gcode.py
@@ -1,7 +1,6 @@
-import unittest
 from unittest.mock import MagicMock, call
-
 import pytest
+
 from octoprint_octodash import OctodashPlugin
 
 # class MockPluginManager:
@@ -29,35 +28,30 @@ def test_fan_sending(match, expected):
 
 
 
-class TestFanGcode(unittest.TestCase):
-    def setUp(self):
-        self.plugin = OctodashPlugin()
-        # self.plugin._plugin_manager = MockPluginManager()
-        self.plugin._plugin_manager= MagicMock()
+@pytest.mark.parametrize("gcode, expected", [
+    # ("M106", (None, None)),
+    # ("M106 P1", (1, None)),
+    ("M106 S255", (None, '255')),
+    ("M106 P1 S128", ('1', '128')),
+    ("M106 P2 S64", ('2', '64')),
+    ("M106 P3 S0", ('3', '0')),
+    # ("M107", (None, '')),
+    # ("M107 P1", ('1', '')),
+])
+def test_good(gcode, expected):
+    plugin = OctodashPlugin()
+    # self.plugin._plugin_manager = MockPluginManager()
 
+    match = plugin._run_gcode_test(gcode)
+    assert match is not None, f"Expected to match: {gcode}"
+    assert match == expected
 
-    def test_good(self):
-        suites = [
-            # ("M106", (None, None)),
-            # ("M106 P1", (1, None)),
-            ("M106 S255", (None, '255')),
-            ("M106 P1 S128", ('1', '128')),
-            ("M106 P2 S64", ('2', '64')),
-            ("M106 P3 S0", ('3', '0')),
-            # ("M107", (None, '')),
-            # ("M107 P1", ('1', '')),
-        ]
-        for gcode, expected in suites:
-          match = self.plugin._run_gcode_test(gcode)
-          self.assertIsNotNone(match, f"Expected to match: {gcode}")
-          self.assertEqual(match, expected)
-
-    # def test_bad(self):
-    #     bad_gcodes = [
-    #         "34343 M106 S255",
-    #         "DSFM106 S255",
-    #         "G10 F1500 E-6.5",
-    #     ]
-    #     for gcode in bad_gcodes:
-    #       match = self.plugin._run_gcode_test(gcode)
-    #       self.assertIsNone(match)
+@pytest.mark.parametrize("gcode", [
+        "34343 M106 S255",
+        "DSFM106 S255",
+        "G10 F1500 E-6.5",
+])
+def test_bad(gcode):
+    plugin = OctodashPlugin()
+    match = plugin._run_gcode_test(gcode)
+    assert match is None, f"Expected not to match: {gcode}"
