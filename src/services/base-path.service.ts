e55278bfa648a75fb6606a56d8de56e35368e9a9
Hilton Shumway <hillshum@gmail.com>
2026-02-25 21:30:53 +0000

    Use logic from base path service in module provider

--- src/services/base-path.service.ts
+++ src/services/base-path.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 
+export const getBase = () => {
+  return window['__baseHref'] || '';
+};
+
 @Injectable()
 export class BasePathService {
   public getBasePath(): string {
-    return window['__baseHref'] || '';
+    return getBase();
   }
 }
