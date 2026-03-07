721437c3cfeadd5d98ad739086c0e6ae88172677
Hilton Shumway <hillshum@gmail.com>
2026-03-07 15:53:20 +0000

    Fix on waiting for octoprint

--- octoprint_octodash/scripts/manage-octodash.sh
+++ octoprint_octodash/scripts/manage-octodash.sh
@@ -4,7 +4,7 @@ octodash_url_prompt="What is the URL of your OctoDash installation? Be sure to i
 octodash_url_default="http://localhost:5000/"
 
 browser_launch_string="chromium-browser --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized" 
-octoprint_suffix="plugin/octodash/"
+octoprint_suffix="/plugin/octodash/"
 
 dependencies="xserver-xorg xinit chromium-browser"
 
