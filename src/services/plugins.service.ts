38e7680bdfdfd83f5b8853db721d444724beaff0
Hilton Shumway <hillshum@gmail.com>
2026-03-19 17:30:38 +0000

    Send auth headers in request for plugins (#5682)

--- src/services/plugins.service.ts
+++ src/services/plugins.service.ts
@@ -5,14 +5,19 @@ import { map } from 'rxjs/operators';
 import { PluginResponse } from 'src/model/octoprint';
 
 import { BasePathService } from './base-path.service';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class PluginsService {
   private http = inject(HttpClient);
   private basePathService = inject(BasePathService);
+  private config = inject(ConfigService);
   public getEnabledPlugins(): Observable<string[]> {
     return this.http
-      .get<PluginResponse>(`${this.basePathService.getBasePath()}/plugin/pluginmanager/plugins`)
+      .get<PluginResponse>(
+        `${this.basePathService.getBasePath()}/plugin/pluginmanager/plugins`,
+        this.config.getHTTPHeaders(),
+      )
       .pipe(map(response => response.plugins.filter(plugin => plugin.enabled).map(plugin => plugin.key)));
   }
 }
