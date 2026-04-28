33e7edf480120d9d375964ac9f19b5819becdad6
Hilton Shumway <hillshum@gmail.com>
2026-04-28 09:12:24 +0100

    Fix race condition with getting selected spools

--- src/components/filament/choose-filament/choose-filament.component.ts
+++ src/components/filament/choose-filament/choose-filament.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { FilamentSpool } from '../../../model';
 import { FilamentService } from '../../../services/filament/filament.service';
@@ -15,7 +16,10 @@ export class ChooseFilamentComponent {
   private currentSpools: (number | null)[];
 
   constructor(public filament: FilamentService) {
-    this.currentSpools = (filament.getCurrentSpools() || []).map(s => s?.id || null);
+    filament
+      .getCurrentSpools()
+      .pipe(map(spools => spools.map(s => s?.id || null)))
+      .subscribe(ids => (this.currentSpools = ids));
   }
 
   public getSpoolWeightLeft(weight: number, used: number): number {
