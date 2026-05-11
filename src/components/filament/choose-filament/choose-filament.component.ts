0a50dc4ed4e6a604f8b799fd1549d1f657cb3925
Hilton Shumway <hillshum@gmail.com>
2026-05-11 18:56:43 +0100

    Initial pass on a basic filter

--- src/components/filament/choose-filament/choose-filament.component.ts
+++ src/components/filament/choose-filament/choose-filament.component.ts
@@ -5,6 +5,8 @@ import { map } from 'rxjs/operators';
 import { FilamentSpool } from '../../../model';
 import { FilamentService } from '../../../services/filament/filament.service';
 
+type SpoolFilter = (spool: FilamentSpool) => boolean;
+
 @Component({
   selector: 'app-filament-choose-spool',
   templateUrl: './choose-filament.component.html',
@@ -16,14 +18,22 @@ export class ChooseFilamentComponent {
 
   private currentSpools: Signal<(number | null)[]>;
 
+  private activeFilters: { [key: string]: SpoolFilter } = {};
+
   constructor(public filament: FilamentService) {
     this.currentSpools = toSignal(filament.getCurrentSpools().pipe(map(spools => spools.map(s => s?.id || null))), {
       initialValue: [],
     });
+
+    this.activeFilters.material = spool => spool.material === 'PLA+';
   }
 
   public getSpoolWeightLeft(weight: number, used: number): number {
     return Math.floor(weight - used);
+  }
+
+  public filterSpools(spools: FilamentSpool[]): FilamentSpool[] {
+    return spools.filter(spool => Object.values(this.activeFilters).every(filter => filter(spool)));
   }
 
   public setSpool(spool: FilamentSpool): void {
