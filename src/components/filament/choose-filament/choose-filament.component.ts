699434344cd46a53e655a83ebb405465f331dd97
Hilton Shumway <hillshum@gmail.com>
2026-06-12 09:36:05 +0100

    Allow filtering and sorting on filament select screen (#5742)
    * Fix error with current spool
    
    Not sure what this affects but was lingering from previous stuff
    
    * Initial pass on a basic filter
    
    * WIP on filtering
    
    * Use onpush in choose-filament component
    
    If nothing else makes dev easier because it's not rerendering on each WS update
    
    * Get it working better
    
    * Initial work on stying the filter modal
    
    * Remove gradient over table
    
    It didn't work and is hard to maintain
    
    * Move filter button
    
    * Add a max weight filter
    
    * Add basic weight slider
    
    * Convert more vars to signals
    
    * Migrate some filamentservice outputs to observables
    
    * Fix default max weight
    
    * Sort vendor and type options
    
    * Move weight label to side
    
    It's hard to read with a finger over it
    
    * Bring down the slider a bit
    
    * Adjust clipping
    
    * Lint fix
    
    * Update locale
    
    * Remove duplicate css rules
    
    * Implement debouncing on weight
    
    * Make sliders bigger
    
    * Locale update
    
    * Tweak modal styles
    
    * WIP on sorting
    
    It kinda works but not consistently and it needs better styling
    
    * WIP on sorting
    
    * Finish up sorting
    
    * Lint fixes
    
    * Locale update

--- src/components/filament/choose-filament/choose-filament.component.ts
+++ src/components/filament/choose-filament/choose-filament.component.ts
@@ -1,30 +1,127 @@
-import { Component, EventEmitter, Output, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs/operators';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  EventEmitter,
+  inject,
+  Output,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
 import { FilamentSpool } from '../../../model';
 import { FilamentService } from '../../../services/filament/filament.service';
+
+enum SortDirection {
+  Asc = 'asc',
+  Desc = 'desc',
+}
+
+interface SpoolFilters {
+  material: WritableSignal<string | null>;
+  vendor: WritableSignal<string | null>;
+  minWeight: WritableSignal<number | null>;
+  maxWeight: WritableSignal<number | null>;
+}
 
 @Component({
   selector: 'app-filament-choose-spool',
   templateUrl: './choose-filament.component.html',
   styleUrls: ['./choose-filament.component.scss', '../filament.component.scss'],
   standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChooseFilamentComponent {
   @Output() spoolChange = new EventEmitter<{ spool: FilamentSpool; skipChange: boolean }>();
 
-  private currentSpools: Signal<(number | null)[]>;
+  public filament: FilamentService = inject(FilamentService);
+  spools = toSignal(this.filament.getFilamentSpools());
 
-  constructor(public filament: FilamentService) {
-    this.currentSpools = toSignal(filament.getCurrentSpools().pipe(map(spools => spools.map(s => s?.id || null))), {
+  private activeFilters: SpoolFilters = {
+    material: signal<string | null>(null),
+    vendor: signal<string | null>(null),
+    minWeight: signal<number>(0),
+    maxWeight: signal<number | null>(null),
+  };
+
+  private activeSort = {
+    key: signal<string | null>(null),
+    direction: signal<SortDirection>(SortDirection.Desc),
+  };
+
+  showFilters = signal(false);
+
+  showSorts = signal(false);
+
+  private debouncedWeights = toSignal(
+    combineLatest([toObservable(this.activeFilters.minWeight), toObservable(this.activeFilters.maxWeight)]).pipe(
+      debounceTime(300),
+    ),
+    {
+      initialValue: [null, null] as [number | null, number | null],
+    },
+  );
+
+  currentSpools: Signal<(number | null)[]> = toSignal(
+    this.filament.getCurrentSpools().pipe(map(spools => spools.map(s => s?.id || null))),
+    {
       initialValue: [],
-    });
-  }
+    },
+  );
 
   public getSpoolWeightLeft(weight: number, used: number): number {
     return Math.floor(weight - used);
   }
+
+  public filteredSpools = computed(() => {
+    const [minWeight, maxWeight] = this.debouncedWeights();
+    const key = this.activeSort.key();
+    const direction = this.activeSort.direction();
+    // null -> no filtering
+    // empty string -> filter for empty values
+    return this.spools()
+      .filter(spool => {
+        if (this.activeFilters.material() === null) {
+          return true;
+        }
+        return this.activeFilters.material() === spool.material;
+      })
+      .filter(spool => {
+        if (this.activeFilters.vendor() === null) {
+          return true;
+        }
+        return this.activeFilters.vendor() === spool.vendor;
+      })
+      .filter(spool => {
+        const weightLeft = this.getSpoolWeightLeft(spool.weight, spool.used);
+        if (minWeight !== null && weightLeft < minWeight) {
+          return false;
+        }
+        if (maxWeight !== null && weightLeft > maxWeight) {
+          return false;
+        }
+        return true;
+      })
+      .sort((a, b) => {
+        if (!key || !direction) {
+          return 0;
+        }
+
+        const asc = SortDirection.Asc as string;
+
+        if (a[key] < b[key]) {
+          return direction === asc ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+          return direction === asc ? 1 : -1;
+        }
+        return 0;
+      });
+  });
 
   public setSpool(spool: FilamentSpool): void {
     setTimeout(() => {
@@ -36,5 +133,30 @@ export class ChooseFilamentComponent {
     setTimeout(() => {
       this.spoolChange.emit({ spool, skipChange: true });
     }, 150);
+  }
+
+  public materials = computed(() => {
+    return Array.from(new Set(this.spools().map(s => s.material))).sort();
+  });
+
+  public manufacturers = computed(() => {
+    return Array.from(new Set(this.spools().map(s => s.vendor))).sort();
+  });
+
+  public maximumWeight = computed(() => {
+    const spools = this.spools();
+    if (!spools) {
+      return 1000;
+    }
+    const weights = spools.map(s => this.getSpoolWeightLeft(s.weight, s.used));
+    return weights.length > 0 ? Math.max(...weights) : 1000;
+  });
+
+  public setMinWeight(value: string): void {
+    this.activeFilters.minWeight.set(value ? parseInt(value, 10) : null);
+  }
+
+  public setMaxWeight(value: string): void {
+    this.activeFilters.maxWeight.set(value ? parseInt(value, 10) : null);
   }
 }
--- src/services/filament/filament.service.ts
+++ src/services/filament/filament.service.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { FilamentSpool } from '../../model';
 import { ConfigService } from '../../services/config.service';
@@ -9,9 +9,9 @@ import { FilamentPluginService } from './filament-plugin.service';
 
 @Injectable()
 export class FilamentService {
-  private filamentSpools: Array<FilamentSpool>;
+  private filamentSpools = new BehaviorSubject<Array<FilamentSpool>>([]);
   private currentSpool = new BehaviorSubject<Array<FilamentSpool>>([]);
-  private loading = true;
+  private loading = new BehaviorSubject<boolean>(true);
 
   constructor(
     private notificationService: NotificationService,
@@ -25,10 +25,10 @@ export class FilamentService {
 
   private loadSpools(): void {
     this.filamentPluginService.getSpools().subscribe({
-      next: (spools: Array<FilamentSpool>) => (this.filamentSpools = spools),
+      next: (spools: Array<FilamentSpool>) => this.filamentSpools.next(spools),
       error: (error: HttpErrorResponse) =>
         this.notificationService.warn($localize`:@@error-spools:Can't load filament spools!`, error.message),
-      complete: () => (this.loading = false),
+      complete: () => this.loading.next(false),
     });
     this.filamentPluginService.getCurrentSpools().subscribe({
       next: (spools: FilamentSpool[]) => this.currentSpool.next(spools),
@@ -37,8 +37,8 @@ export class FilamentService {
     });
   }
 
-  public getFilamentSpools(): Array<FilamentSpool> {
-    return this.filamentSpools;
+  public getFilamentSpools(): Observable<Array<FilamentSpool>> {
+    return this.filamentSpools.asObservable();
   }
 
   public getCurrentSpools() {
@@ -46,11 +46,11 @@ export class FilamentService {
   }
 
   public getCurrentSpool(tool: number): FilamentSpool {
-    return this.currentSpool && this.currentSpool[tool];
+    return this.currentSpool.getValue()[tool];
   }
 
-  public getLoading(): boolean {
-    return this.loading;
+  public getLoading(): Observable<boolean> {
+    return this.loading.asObservable();
   }
 
   public setSpool(spool: FilamentSpool, tool: number): Promise<void> {
